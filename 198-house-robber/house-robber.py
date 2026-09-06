5f8a48b58c6d02b5a58fac92c89d338a7be0b488
Rithvik <95231603+Rithvik-d@users.noreply.github.com>
2026-09-06 15:54:45 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.37%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rob(self, nums):
+        rob1, rob2 = 0,0
+        
+        for num in nums:
+            temp = max(rob1 + num ,rob2)
+            rob1= rob2
+            rob2 = temp
+
+        return rob2
+        
